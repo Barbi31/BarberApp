40b5f590eb6478683395b7d93f98b00ec9c32807
Barbi31 <150376287+Barbi31@users.noreply.github.com>
2025-03-24 21:41:04 +0200

    10

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,5 +1,5 @@
 
-// Firebase configuration and initialization
+// Firebase config for District45
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
