526e015397260888ffab66df219a58f07d8b8c52
Barbi31 <150376287+Barbi31@users.noreply.github.com>
2025-03-25 19:49:42 +0200

    16

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,18 +1,30 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOmhHpvoffYO4NrG2PMAIX_GAICmtgCX0",
-  authDomain: "district45-de4ec.firebaseapp.com",
-  projectId: "district45-de4ec",
-  storageBucket: "district45-de4ec.firebasestorage.app",
-  messagingSenderId: "593842934683",
-  appId: "1:593842934683:web:aeceeb1bea68eb9b27873e",
-  measurementId: "G-W7Z1LHHYZN"
+    apiKey: "AIzaSyDOmhHpvoffYO4NrG2PMAIX_GAICmtgCX0",
+    authDomain: "district45-de4ec.firebaseapp.com",
+    projectId: "district45-de4ec",
+    storageBucket: "district45-de4ec.appspot.com",
+    messagingSenderId: "593842934683",
+    appId: "1:593842934683:web:aeceeb1bea68eb9b27873e",
+    measurementId: "G-W7Z1LHHYZN"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth, signInWithEmailAndPassword };
+window.login = function () {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const error = document.getElementById('error');
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            window.location.href = 'book.html';
+        })
+        .catch((err) => {
+            error.textContent = "Login failed: " + err.message;
+        });
+};
