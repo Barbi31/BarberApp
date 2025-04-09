dddf6cb19c27a26ca475f75fe85c67a29f6de93e
Barbi31 <150376287+Barbi31@users.noreply.github.com>
2025-04-09 22:02:21 +0300

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,3 +1,4 @@
+// Firebase SDK importálása
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
@@ -7,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDOmhHpvoffYO4NrG2PMAIX_GAICmtgCX0",
   authDomain: "district45-de4ec.firebaseapp.com",
   projectId: "district45-de4ec",
-  storageBucket: "district45-de4ec.appspot.com",
+  storageBucket: "district45-de4ec.firebasestorage.app",
   messagingSenderId: "593842934683",
   appId: "1:593842934683:web:aeceeb1bea68eb9b27873e",
   measurementId: "G-W7Z1LHHYZN"
@@ -15,8 +16,10 @@ const firebaseConfig = {
 
 // Firebase inicializálás
 const app = initializeApp(firebaseConfig);
+
+// Firebase autentikáció és Firestore inicializálás
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Exportálás
+// Exportálás, hogy más fájlok is használhassák
 export { app, auth, db };
